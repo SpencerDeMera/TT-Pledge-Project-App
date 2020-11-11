372be2f6e911d99d8bb914d140d8338739e50964
Spencer5515 <71044889+Spencer5515@users.noreply.github.com>
2020-11-10 23:46:45 -0800

    Add files via upload

---
+++ DontDestroyAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyAudio : MonoBehaviour {
+
+    static bool AudioBegin = false;
+
+    void Awake() {
+        if (!AudioBegin) {
+            // audio.Play();
+            DontDestroyOnLoad(transform.gameObject);
+            AudioBegin = true;
+        } // if
+    } // Awake function
+
+    void Update () {
+        if (Application.loadedLevelName == "MainMenu") {
+            Destroy(this.gameObject);
+            AudioBegin = false;
+        } // else
+    } // Update function
+
+} // DontDestroyAudio function
