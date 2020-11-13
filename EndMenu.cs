23e5d47f20b2830ea800970835003153f3dd18ad
Spencer5515 <71044889+Spencer5515@users.noreply.github.com>
2020-11-13 15:19:20 -0800

    Update EndMenu.cs

--- EndMenu.cs
+++ EndMenu.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections;
+// EndMenu File
+//  Author(s): Spencer DeMera
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
